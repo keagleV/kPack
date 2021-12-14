6cb54c1100041aa3f3d62ffcb8db556f4971b48c
keagleV <kiarash_sedghi@protonmail.com>
2021-12-14 18:59:59 +0330

    adding to ea algorithm

--- kpackea.py
+++ kpackea.py
@@ -14,3 +14,32 @@ class EvolAlgoParams:
 
 
 
+class KpackEA:
+	'''
+		This class implements all the necessary function for the EA in the packing
+		problem.
+	'''
+
+
+	def __init__ (self,objects):
+
+		# Dictionary of all the objects
+		self.objects=objects
+
+	def initial_filtering(self):
+		'''
+			This function filters out some of the objects that cannot be
+			placed in the container in any condition. Such conditions are
+
+			1- the weight of the object exceeds the container's weight
+			2- the area of the object exceeds the container's area
+		'''
+		pass
+
+
+		
+
+
+
+
+
